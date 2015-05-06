714e7a93874c3df0f37dbfc3833dda1711ff8ad1
veitseb <johannessebastian.veit@hs-augsburg.de>
2015-05-06 21:24:09 +0200

    Changes to be committed: 	modified:   HalloWelt.java

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,6 +1,10 @@
 public class HalloWelt{
 
 	public static void main(String[] args){
+		int i;
+		for(i=0; i < 10; i++){
+			System.out.println("i:", i);
+		}
 		System.out.println("Hallo Welt!");
 	}
 
